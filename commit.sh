4f4cb4b82217546d2bc1e286a9a4e19e6d17f114
ndnam198 <ndnam198@gmail.com>
2020-10-25 14:53:03 +0700

    [10/25/20 14:53:03]:

--- commit.sh
+++ commit.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+timestamp=$(date +"%D %T")
+message=$1
 git add -A;
-git commit -m "$1";
+git commit -m "[$timestamp]: $message"
 git push https://github.com/ndnam198/STM32_UART_STRING_RX_IT.git master
+
